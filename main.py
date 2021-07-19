e8449b7f6d0dd62a096cef2cfdfa89c5431af04d
raihaan123 <raihaan.usman@gmail.com>
2021-07-19 23:04:45 +0100

    mode info

--- main.py
+++ main.py
@@ -27,34 +27,44 @@ with left:
     # Regenerative Braking for Hyperloop
     ### [MIT Hyperloop III](https://www.mithyperloop.mit.edu/) x [ICLoop](https://www.linkedin.com/company/icloop) - European Hyperloop Week 2021
     '''
+
 with centre:
     st.write("")
     st.write("")
     '''
-    ### Link to our poster!
+    ### Scan for our [poster](https://www.overleaf.com/read/hssgskqhcqwz)!
     '''
+
 with right:
     st.image('qr-code.png', width=150)
 
-
 # Mode choices (1 or 4)
-fpath = ''
-x = st.selectbox("Select an operating mode to view simulation results", ['Mode 1 - 100 RPM','Mode 1 - 300 RPM','Mode 4 - Regenerative Braking'])
+left, middle, right = st.beta_columns([2,2,3])
 
-if x == 'Mode 1 - 100 RPM': fpath = 'Mode1_100.mat'
-elif x == 'Mode 1 - 300 RPM': fpath = 'Mode1_300.mat'
-else: fpath = 'Mode4.mat'
+with left:
+    fpath = ''
+    x = st.selectbox("Select an operating mode to view simulation results", ['Mode 1 - 100 RPM','Mode 1 - 300 RPM','Mode 4 - Regenerative Braking'])
+
+    if x == 'Mode 1 - 100 RPM': fpath = 'Mode1_100.mat'; x=1
+    elif x == 'Mode 1 - 300 RPM': fpath = 'Mode1_300.mat'; x=2
+    else: fpath = 'Mode4.mat'; x=3
+
+with right:
+    # Documentation for the app
+    with st.beta_expander("What is this mode about?"):
+        if x != 3:
+            """
+            Mode 1 is the standard pushing state for the pod - the battery system solely powers the motor. The RPM represents different setpoints for the system.
+            """
+        else:
+            """
+            Mode 4 is regenerative braking with the battery system as the power sink.
+            """
 
 # Load mat-file
 mat = loadmat(f'MATLAB_files/{fpath}')
 
 # Conversion to pandas dataframe
-# try:
-#     df = pd.DataFrame(np.hstack((mat['t'],mat['bat_soc'], mat['rotor_speed'], mat['rotor_torque'], mat['sc_soc'])),
-#                         columns=['Time (s)', 'Battery SoC', 'Rotor Speed', 'Rotor Torque', 'Supercapacitor SoC']).set_index('Time (s)')
-
-# # Rotor torques throw an error caused by increased precision - save to new dataframe
-# except ValueError:
 df = pd.DataFrame(np.hstack((mat['t'],mat['bat_soc'], mat['rotor_speed'], mat['sc_soc'])),
                     columns=['Time (s)', 'Battery SoC', 'Rotor Speed', 'Supercapacitor SoC']).set_index('Time (s)')
 
@@ -85,29 +95,6 @@ rotor_speed = go.Figure(
         yaxis=dict(title='RPM', showgrid=False, zeroline=False)
     )
 )
-# try:
-#     rotor_torque = go.Figure(
-#         data=[
-#             go.Scatter(x=df.index, y=df['Rotor Torque'], name='Rotor Torque'),
-#         ],
-#         layout=go.Layout(
-#             title='Rotor Torque',
-#             xaxis=dict(title='Time (s)', showgrid=False, zeroline=False),
-#             yaxis=dict(title='Nm', showgrid=False, zeroline=False)
-#         )
-#     )
-#     super_soc = go.Figure(
-#         data=[
-#             go.Scatter(x=df.index, y=df['Supercapacitor SoC'], name='Supercapacitor SoC'),
-#         ],
-#         layout=go.Layout(
-#             title='Supercapacitor SoC',
-#             xaxis=dict(title='Time (s)', showgrid=False, zeroline=False),
-#             yaxis=dict(title='SoC (%)', showgrid=False, zeroline=False, range=[90,100])
-#         )
-#     )
-# except:
-
 rotor_torque = go.Figure(
     data=[
         go.Scatter(x=df_torque.index, y=df_torque['Rotor Torque'], name='Rotor Torque'),
@@ -119,8 +106,9 @@ rotor_torque = go.Figure(
     )
 )
 
-if x == 'Mode 1 - 100 RPM': ran = [90, 100]
-elif x == 'Mode 1 - 300 RPM': ran = [90, 100]
+# Selecting SoC range for demo - won't scale well but will do for the presentation
+if x == 1: ran = [90, 100]
+elif x == 2: ran = [90, 100]
 else: ran = [0, 15]
 
 super_soc = go.Figure(
@@ -152,4 +140,3 @@ with st.beta_expander("View raw data!"):
         with two:
             st.write(df_torque)
     except: pass
-
